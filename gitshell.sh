930b39f806660102a31b5cd4baeee3a7c514c9d9
Mothukuri sairam <mothukurisairam@gmail.com>
2021-11-11 15:25:13 +0000

    modified files

--- gitshell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -n "enter the prefence output filename :"
-read x
-
-git show --name-only -r HEAD | sed -n '7,100p'>> $x
-
-
-cat $x
--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-adad
-ahdhg
-new Line
