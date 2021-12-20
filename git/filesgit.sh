f3271361b1644c33f4ddbe77974f11a66b1c063c
Mothukurisairam <93304271+Mothukurisairam@users.noreply.github.com>
2021-12-20 14:55:52 +0530

    Update filesgit.sh

--- git/filesgit.sh
+++ git/filesgit.sh
@@ -3,6 +3,7 @@
 #Please provide an input file name, which will be created as the new file and save the names in that file.
 echo -n "enter file name :"
 read x
+
 #Taking input from the git commit and selecting files from the recent commit.
 git show --name-only -r HEAD | sed -n '7,50p' >> $x
 
